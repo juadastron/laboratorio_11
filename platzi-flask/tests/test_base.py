112f368c8331ca88504c623ad73030bb1618dc13
juadastron <juan.d.carreno@unl.edu.ec>
2024-07-11 11:59:21 -0500

    ajustes en archivo test para mandar nofiticaciones por flask

--- platzi-flask/tests/test_base.py
+++ platzi-flask/tests/test_base.py
@@ -2,8 +2,7 @@ from flask_testing import TestCase
 from flask import current_app, url_for
 #Se importa la aplicacion de Flask
 
-from ..main import app
-
+from main import app
 class MainTest(TestCase):
     # Se implementa el metodo create_app que vive en la clase TestCase y tiene que regresar una aplicacion de Flask
     def create_app(self):
